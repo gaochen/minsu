27641285436d766db607d5316f24085980bea962
gaochen <gc_andrew@sina.com>
2018-03-27 18:21:32 +0800

    1.1版 体验版

--- mitu/pages/home1/home.js
+++ mitu/pages/home1/home.js
@@ -1,13 +1,5 @@
 Page({
   data: {
-    markers: [{
-      iconPath: "/images/map.jpg",
-      id: 0,
-      latitude: 30.653500,
-      longitude: 104.086970,
-      width: 50,
-      height: 50
-    }]
   },
   toPicture: function() {
     wx.navigateTo({
--- mitu/pages/map/map.js
+++ mitu/pages/map/map.js
@@ -1,23 +1,31 @@
 
 Page({
   data: {
-    // circles: [{
-    //   longitude: 104.086970,
-    //   latitude: 30.653500,
-    //   color: '#008B8B33',
-    //   fillColor: '#008B8B',
-    //   radius: 100,
-    //   strokeWidth: 50
-    // }],
-    markers: [{
-      iconPath: "/images/map.jpg",
-      id: 0,
-      latitude: 30.653500,
+    circles: [{
       longitude: 104.086970,
-      width: 50,
-      height: 50
+      latitude: 30.653500,
+      color: '#ffffff',
+      fillColor: '#008B8B66',
+      radius: 400,
+      strokeWidth: 0
     }]
+    // markers: [{
+    //   iconPath: "/images/map.jpg",
+    //   id: 0,
+    //   latitude: 30.653500,
+    //   longitude: 104.086970,
+    //   width: 50,
+    //   height: 50
+    // }]
   },
   onLoad: function () {
+  },
+  openLocation: function() {
+    wx.openLocation({  
+      latitude: 30.653500,  
+      longitude: 104.086970,  
+      name: '朗御',  
+      address:'锦江区大慈寺路3号'  
+    })
   }
 })
