2047ba4e82ec8934b40fbea3b8a1eaf4f38d5abe
gaochen <gc_andrew@sina.com>
2018-05-24 16:30:47 +0800

    git status

---
+++ mitu/api/index.js
@@ -0,0 +1,8 @@
+const ip = 'https://api.forbiger.com/'
+
+const api = {
+  getUserOpenId: `${ip}Wx/getUserOpenId`,
+  login: `${ip}CustomerUser/login`
+}
+
+export default api
--- mitu/app.js
+++ mitu/app.js
@@ -1,46 +1,45 @@
+import api from './api/index'
+import ajax from './api/ajax'
+
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
+        if (res.code) {
+          // 获取用户openId
+          ajax({
+            url: api.getUserOpenId,
+            method: 'POST',
+            data: {
+              jscode: res.code,
+              mini_key: this.globalData.mini_key
+            },
             success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+              this.globalData.openId = res.data.data.openid
             }
+          }).then((res) => {
+            // 获取openId成功之后，判断该用户是否已存在
+            return ajax({
+              url: api.login,  
+              method: 'POST',
+              data: {
+                open_id: this.globalData.openId,
+                mini_key: this.globalData.mini_key
+              }
+            })
+          }).catch((error) => {
+            console.log(error)
           })
         }
       }
     })
   },
   globalData: {
-    roomList: {
-      '1': {
-        'title': 'ROOM1.Cure'
-      },
-      '2': {
-        'title': 'ROOM2.Story'
-      }
-    }
+    mini_key: 'customer_dev_v1',
+    openId: null,
+    token: null
   }
 })
